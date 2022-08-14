9bfa25580d7e8e89fb65688bad32dde7e0692e91
hasai <hasaikaz@gmail.com>
2022-08-14 16:13:06 +0900

    index.blade.php編集

---
+++ backend/resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>バトオペ2カスタムパーツシュミレーター</title>
+</head>
+
+<body>
+  <h1>バトオペ2カスタムパーツシュミレーター</h1>
+</body>
+
+</html>
